3b62320a7e5a77df0ee3dbd220612eebbe5af477
sonu2192 <37955868+sonu2192@users.noreply.github.com>
2018-04-14 23:57:42 +0530

    Update roundrobin.c

--- roundrobin.c
+++ roundrobin.c
@@ -10,14 +10,24 @@ struct process
 	int rt;
 	int tat;
 	int exe;
+	int ct;
 }s[10];
 struct process ready[10];
+struct process p_idle;
 int t=0,r,n;
 struct process temp;
+struct process temp1;
 struct process protemp;
+struct process protemp1;
 main()
 {
-	int i,j,k,l,o,m,size=0,quant=10,istart=0;
+	p_idle.at=0;
+	p_idle.bt=10000;
+	p_idle.id=0;
+	p_idle.pri=0;
+	p_idle.rt=p_idle.bt;
+	ready[0]=p_idle;
+	int u,i,j,k,l,o,m,size=0,quant=10,istart=0;
 	printf("\nEnter the number of process:");
 	scanf("%d",&n);
 	r=n;
@@ -33,6 +43,7 @@ main()
 		scanf("%d",&s[i].pri);
 		s[i].exe=0;
 	}
+	 printf("\nGanttChart for every second is:");
 	 while(r!=0)
 	 {
 	 	for(i=0;i<n;i++)
@@ -60,9 +71,14 @@ main()
 		 			ready[k+1]=temp;
 				}
 			}
-			
 		 }
 	   }
+	   if(t>0&&size==0)
+	   {
+	   	printf("P%d  ",p_idle.id);
+	   }
+	   else
+	   {
 	    if(t%10==0&&ready[1].rt!=0)
 	    {
 	    	protemp=ready[0];
@@ -75,18 +91,37 @@ main()
 	    }
 	     if(ready[0].rt!=0)
 		 {
-		 	printf("%d",ready[0].id);
+		 	printf("P%d  ",ready[0].id);
 		 	ready[0].rt--;
 		 }
 		 if(ready[0].rt==0&&t!=0)
 		 {
+		 	int y;
+		 	for(y=0;y<n;y++)
+		 	{
+		 		if(s[y].id==ready[0].id)
+		 		{
+		 			s[y].ct=t+1;
+		 			break;
+				}
+			}
+		 	int q;
+		 	for(q=1;q<size;q++)
+		 	{
+		 		ready[q-1]=ready[q];
+			}
+			 size--;
 		 	r--;
 		 }
+	}
 		 t++;
     }
     int z;
     for(z=0;z<n;z++)
     {
-    	printf("\n%d\n",t);
+    	s[z].wt=s[z].ct-s[z].bt-s[z].at;
+    	s[z].tat=s[z].wt+s[z].bt;
+    	printf("\nWaiting time:%d\n",s[z].wt);
+    	printf("\nTurnAroundTime:%d\n",s[z].tat);
     }
 }
