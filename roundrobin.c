bb3f63173f9f3a9fe3a430347522fa0f58600c24
sonu2192 <37955868+sonu2192@users.noreply.github.com>
2018-04-15 16:17:27 +0530

    Update roundrobin.c

--- roundrobin.c
+++ roundrobin.c
@@ -2,73 +2,67 @@
 #include<conio.h>
 struct process
 {
-	int bt;
-	int wt;
+	int btime;
+	int wtime;
 	int id;
 	int pri;
-	int at;
-	int rt;
-	int tat;
+	int atime;
+	int rtime;
+	int tatime;
 	int exe;
-	int ct;
+	int ctime;
 }s[10];
-struct process ready[10];
+struct process readyq[10];
 struct process p_idle;
-int t=0,r,n;
+int t=0,rem,n;
 struct process temp;
 struct process temp1;
 struct process protemp;
 struct process protemp1;
 main()
 {
-	p_idle.at=0;
-	p_idle.bt=10000;
+	p_idle.atime=0;
+	p_idle.btime=10000;
 	p_idle.id=0;
 	p_idle.pri=0;
-	p_idle.rt=p_idle.bt;
-	ready[0]=p_idle;
+	p_idle.rtime=p_idle.btime;
+	readyq[0]=p_idle;
 	int u,i,j,k,l,o,m,size=0,quant=10,istart=0;
-	printf("\nEnter the number of process:");
+	printf("\nEnter the Total number of process:");
 	scanf("%d",&n);
-	r=n;
+	rem=n;
 	for(i=0;i<n;i++)
 	{
 		s[i].id=i+1;
-		printf("\nEnter the arrival time of process p%d:",i+1);
-		scanf("%d",&s[i].at);
-		printf("\nEnter the burst time of the process p%d:",i+1);
-		scanf("%d",&s[i].bt);
-		s[i].rt=s[i].bt;
+		printf("\nEnter arrival time of p%d:",i+1);
+		scanf("%d",&s[i].atime);
+		printf("\nEnter burst time of p%d:",i+1);
+		scanf("%d",&s[i].btime);
+		s[i].rtime=s[i].btime;
 		printf("Enter the priority of the process p%d:",i+1);
 		scanf("%d",&s[i].pri);
 		s[i].exe=0;
 	}
 	 printf("\nGanttChart for every second is:");
-	 while(r!=0)
+	 while(rem!=0)
 	 {
 	 	for(i=0;i<n;i++)
 	 	{
-	 	 if(s[i].at==t&&s[i].exe==0)
+	 	 if(s[i].atime==t&&s[i].exe==0)
 	 	 {
-	 		ready[size].id=s[i].id;
-	 		ready[size].at=s[i].at;
-	 		ready[size].bt=s[i].bt;
-	 		ready[size].pri=s[i].pri;
-	 		ready[size].rt=s[i].rt;
-	 		ready[size].wt=s[i].wt;
+	 		readyq[size]=s[i];
 	 		s[i].exe=1;
 	 		size++;
-	 		istart=1;
 		 }
 		 for(j=0;j<size-1;j++)
 		 {
 		 	for(k=0;k<j-size-1;k++)
 		 	{
-		 		if(ready[k].pri<ready[k+1].pri)
+		 		if(readyq[k].pri<readyq[k+1].pri)
 		 		{
-		 			temp=ready[k];
-		 			ready[k]=ready[k+1];
-		 			ready[k+1]=temp;
+		 			temp=readyq[k];
+		 			readyq[k]=readyq[k+1];
+		 			readyq[k+1]=temp;
 				}
 			}
 		 }
@@ -79,39 +73,39 @@ main()
 	   }
 	   else
 	   {
-	    if(t%10==0&&ready[1].rt!=0)
+	    if(t%10==0&&readyq[1].rtime!=0)
 	    {
-	    	protemp=ready[0];
+	    	protemp=readyq[0];
 	    	int p;
 	    	for(p=1;p<size;p++)
 	    	{
-	    		ready[p-1]=ready[p];
+	    		readyq[p-1]=readyq[p];
 			}
-			ready[size-1]=protemp;
+			readyq[size-1]=protemp;
 	    }
-	     if(ready[0].rt!=0)
+	     if(readyq[0].rtime!=0)
 		 {
-		 	printf("P%d  ",ready[0].id);
-		 	ready[0].rt--;
+		 	printf("P%d  ",readyq[0].id);
+		 	readyq[0].rtime--;
 		 }
-		 if(ready[0].rt==0&&t!=0)
+		 if(readyq[0].rtime==0&&t!=0)
 		 {
 		 	int y;
 		 	for(y=0;y<n;y++)
 		 	{
-		 		if(s[y].id==ready[0].id)
+		 		if(s[y].id==readyq[0].id)
 		 		{
-		 			s[y].ct=t+1;
+		 			s[y].ctime=t+1;
 		 			break;
 				}
 			}
 		 	int q;
 		 	for(q=1;q<size;q++)
 		 	{
-		 		ready[q-1]=ready[q];
+		 		readyq[q-1]=readyq[q];
 			}
 			 size--;
-		 	r--;
+		 	rem--;
 		 }
 	}
 		 t++;
@@ -119,9 +113,9 @@ main()
     int z;
     for(z=0;z<n;z++)
     {
-    	s[z].wt=s[z].ct-s[z].bt-s[z].at;
-    	s[z].tat=s[z].wt+s[z].bt;
-    	printf("\nWaiting time:%d\n",s[z].wt);
-    	printf("\nTurnAroundTime:%d\n",s[z].tat);
+    	s[z].wtime=s[z].ctime-s[z].btime-s[z].atime;
+    	s[z].tatime=s[z].wtime+s[z].btime;
+    	printf("\nWaiting time:%d\n",s[z].wtime);
+    	printf("\nTurnAroundTime:%d\n",s[z].tatime);
     }
 }
